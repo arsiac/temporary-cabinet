bd8446bc326acbd15aed279d817853b22af21795
arsiac <arsiachou@foxmail.com>
2025-10-21 22:19:38 +0800

    feat: add request and response compression middleware

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,10 +1,17 @@
-mod test;
-
 /// Backend router
-pub fn router() -> axum::Router<ServerState> {
-    axum::Router::new().nest("/test", test::router())
+pub fn router<S>() -> axum::Router<S>
+where
+    S: Clone + Send + Sync + 'static,
+{
+    axum::Router::new().route("/ping", axum::routing::get(ping))
 }
 
 /// Backend server state
 #[derive(Clone)]
 pub struct ServerState;
+
+/// ping the server
+/// GET /api/ping
+pub(crate) async fn ping() -> String {
+    "pong".to_string()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
 mod arg;
 mod init;
 
+/// Index file
 static INDEX_FILE: &str = "index.html";
 
+/// Application entrypoint
+/// - Initialization Log (simple_logger)
+/// - Build routes
+/// - Start the server and listen for the ports in the configuration
 #[tokio::main]
 async fn main() {
     let args = arg::parse();
     init::initialize_logger(args.debug);
-
-    let state = api::ServerState;
-    let static_service = axum_embed::ServeEmbed::<web::WebAssets>::with_parameters(
-        Some(INDEX_FILE.to_string()),
-        axum_embed::FallbackBehavior::Redirect,
-        Some(INDEX_FILE.to_string()),
-        );
-    let app = axum::Router::new()
-    .nest("/api", api::router())
-    .fallback_service(static_service)
-    .with_state(state);
 
     let serv_addr = format!("{}:{}", args.host, args.port);
     let listener = tokio::net::TcpListener::bind(&serv_addr).await;
@@ -26,5 +20,25 @@ async fn main() {
         return;
     }
     log::info!("Serving on {}", &serv_addr);
-    axum::serve(listener.unwrap(), app).await.unwrap();
+    axum::serve(listener.unwrap(), router()).await.unwrap();
+}
+
+/// Merge front-end and back-end routes and configure middleware
+fn router() -> axum::Router {
+    use tower_http::{compression::CompressionLayer, decompression::RequestDecompressionLayer};
+
+    let state = api::ServerState;
+    let static_service = axum_embed::ServeEmbed::<web::WebAssets>::with_parameters(
+        Some(INDEX_FILE.to_string()),
+        axum_embed::FallbackBehavior::Redirect,
+        Some(INDEX_FILE.to_string()),
+    );
+    axum::Router::new()
+        .nest("/api", api::router().with_state(state))
+        .fallback_service(static_service)
+        .layer(
+            tower::ServiceBuilder::new()
+                .layer(RequestDecompressionLayer::new())
+                .layer(CompressionLayer::new()),
+        )
 }
